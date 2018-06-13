b18b755caf6033a9ad3ba3234e312236617394c0
xrsheeran <xqu25@wisc.edu>
2018-06-13 14:39:06 -0500

    Updates of LoadResults

--- ProteoformSweet.xaml.cs
+++ ProteoformSweet.xaml.cs
@@ -2,31 +2,54 @@
 using System.Data;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Win32;
 using ProteoformSuiteGUI;
 
-
-namespace ProteoformWPFSuite
+/*
+ * - MDI Form replaced by user control
+ * - MDI Parent replaced by the getparentwindow (private)
+     */
+namespace ProteoWPFSuite
 {
     /// <summary>
     /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class ProteoformSweet : UserControl
+    public partial class ProteoformSweet : UserControl, IParentMDI
     {
+
+        #region Public Fields
+        
+        #region MDI Iterface
+        public HashSet<String> MDIChildren
+        {
+            get;
+            set;
+        }
+        public void CloseEvent(ITabbedMDI sender, EventArgs e)
+        {
+            MDIChildren.Remove(sender.UniqueTabName);
+
+        }
+        #endregion
+
+        public LoadResults loadResults = new LoadResults();
+        //public RawExperimentalComponents rawExperimentalComponents = new RawExperimentalComponents();
+
+        public List<ISweetForm> forms = new List<ISweetForm>();
+
+        #endregion
+
         #region Private Fields
-        ISweetForm current_form;
+        ISweetForm current_Tab;
         SaveFileDialog saveExcelDialog;
+        #endregion
+
+        #region Private Methods
+        
         #endregion
 
         public ProteoformSweet()
@@ -34,12 +57,10 @@ namespace ProteoformWPFSuite
             InitializeComponent();
         }
 
-        /**
-         * !!!!!!!!!!!!!!Problem: MDI Application look may not be supported; TBA what the application looks
-         **/
+        
         private void ExportTables_Click(object sender, RoutedEventArgs e)
         {
-            List<DataTable> data_tables = current_form.SetTables();
+            List<DataTable> data_tables = current_Tab.SetTables();
 
             if (data_tables == null)
             {
@@ -47,18 +68,22 @@ namespace ProteoformWPFSuite
                 return;
             }
             ProteoformSuiteGUI.ExcelWriter writer = new ProteoformSuiteGUI.ExcelWriter();
-            writer.ExportToExcel(data_tables, (current_form as Window).Name);
-            SaveExcelFile(writer, (current_form as Window).Name +"_table.xlsx");
+            writer.ExportToExcel(data_tables, (current_Tab as UserControl).Name);
+            SaveExcelFile(writer, (current_Tab as UserControl).Name +"_table.xlsx");
         }
+
+        /**
+         * MDI Missing in WPF; Used Tabbed MDI instead; See README.md
+         **/
         private void exportAllTablesToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            /*ExcelWriter writer = new ExcelWriter();
-            if (MessageBox.Show("Will prepare for export. This may take a while.", "Export Data", MessageBoxButtons.OKCancel) == DialogResult.Cancel) return;
+        { 
+            ExcelWriter writer = new ExcelWriter();
+            if (MessageBox.Show("Will prepare for export. This may take a while.", "Export Data", MessageBoxButton.OKCancel) == MessageBoxResult.Cancel) return;
             Parallel.ForEach(forms, form => form.SetTables());
-            writer.BuildHyperlinkSheet(forms.Select(sweet => new Tuple<string, List<DataTable>>((sweet as Form).Name, sweet.DataTables)).ToList());
-            Parallel.ForEach(forms, form => writer.ExportToExcel(form.DataTables, (form as Form).Name));
-            if (MessageBox.Show("Finished preparing. Ready to save? This may take a while.", "Export Data", MessageBoxButtons.OKCancel) == DialogResult.Cancel) return;
-            SaveExcelFile(writer, (current_form as Form).MdiParent.Name + "_table.xlsx");*/
+            writer.BuildHyperlinkSheet(forms.Select(sweet => new Tuple<string, List<DataTable>>((sweet as Window).Name, sweet.DataTables)).ToList());
+            Parallel.ForEach(forms, form => writer.ExportToExcel(form.DataTables, (form as Window).Name));
+            if (MessageBox.Show("Finished preparing. Ready to save? This may take a while.", "Export Data", MessageBoxButton.OKCancel) == MessageBoxResult.Cancel) return;
+            SaveExcelFile(writer, MDIHelpers.getParentWindow(current_Tab as UserControl).Name + "_table.xlsx"); //get the window hosting tabcontrol, which hosts usercontrol
         }
         private void SaveExcelFile(ExcelWriter writer, string filename)
         {
